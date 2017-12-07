e6d64888cb11e4255f8cb569156c23f8d2a3e695
chongaz0717 <chongaz0717@thundersoft.com>
2017-12-07 17:09:09 +0800

    导入pom文件

---
+++ spring/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class HelloWorld {
+    private String message;
+}
---
+++ spring/src/main/java/MainApp.java
@@ -0,0 +1,14 @@
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@Slf4j
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        log.info("111");
+        System.out.println(obj.getMessage());
+    }
+}
