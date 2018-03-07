de0bfc041dff936e27608e98e117be8457fa58d0
chongaizhen <18363999928@163.com>
2018-03-07 22:14:35 +0800

    修改entity类采用lombok注解的方式

--- springboot/src/main/java/science/jiangqi/entity/User.java
+++ springboot/src/main/java/science/jiangqi/entity/User.java
@@ -1,5 +1,10 @@
 package science.jiangqi.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class User {
     private String username;
 
@@ -8,36 +13,4 @@ public class User {
     private String sex;
 
     private String authority;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username == null ? null : username.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex == null ? null : sex.trim();
-    }
-
-    public String getAuthority() {
-        return authority;
-    }
-
-    public void setAuthority(String authority) {
-        this.authority = authority == null ? null : authority.trim();
-    }
 }
